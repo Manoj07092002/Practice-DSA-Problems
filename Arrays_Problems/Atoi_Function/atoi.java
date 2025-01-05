ae8a1567d11cd87507fd44337bb0a4c6846dad93
Manoj M <131946059+Manoj07092002@users.noreply.github.com>
2025-01-05 18:20:24 +0530

    Add files via upload

---
+++ Arrays_Problems/Atoi_Function/atoi.java
@@ -0,0 +1,50 @@
+class atoi{
+
+    public static void main(String args []){
+
+
+        // convert string into integer.
+
+        String str = "-123";
+
+        int res = atoi1(str);
+
+        System.out.println(res); 
+    }
+    static int atoi1(String s){
+        
+        int sign = 0 ;
+        int res =0;
+
+        for(int i =0 ;i <s.length(); i++){
+
+            if(i == 0 && s.charAt(i) == 45){
+                sign =1;
+                continue;
+            
+            }
+
+            if(s.charAt(i) >= '0' && s.charAt(i) <= '9'){
+
+                if(i == 0){
+                    res = res + (s.charAt(i) - 48);
+
+                }
+                else{
+
+                    res = (res*10) + (s.charAt(i) - 48);
+                }
+            }
+            else{
+                return -1;
+            }
+        }
+
+        if(sign ==1){
+            res = 0 -res;
+
+        }
+        return res;
+    }
+
+}
